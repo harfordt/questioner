552a44b74a5ef2c3660cd6b914a2499362397bf2
harfordt <tim.harford@wgc.school.nz>
2019-09-30 21:14:41 +1300

    Tutorial part 4 - database

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,7 @@
 from flask import render_template, flash, redirect, url_for
-from app import app
+
+from app import app, db  # db maybe shouldn't be here
+from app.models import User, Post  # this maybe shouldn't be here
 from app.forms import LoginForm
 
 
@@ -17,3 +19,8 @@ def login():
         flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
         return redirect(url_for('index'))
     return render_template('login.html', form=form)
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
