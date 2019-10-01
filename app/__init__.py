d28e3ce08d1b13219279f92427e2e3bb1ba5fd24
harfordt <tim.harford@wgc.school.nz>
2019-10-01 20:56:56 +1300

    Better error handling and logging

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,10 @@ from flask_bootstrap import Bootstrap
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
+import logging
+from logging.handlers import RotatingFileHandler
+import os
+
 app = Flask(__name__)
 app.config.from_object(Config)
 bootstrap = Bootstrap(app)
@@ -15,4 +19,17 @@ bcrypt = Bcrypt(app)
 login = LoginManager(app)
 login.login_view = 'login'
 
-from app import routes, models
+if not app.debug:
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240,
+                                       backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Microblog startup')
+
+from app import routes, models, errors
