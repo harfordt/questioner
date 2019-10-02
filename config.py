b2314aa623a948f476416e2eaaf0464d718ebdb8
harfordt <tim.harford@wgc.school.nz>
2019-10-02 14:39:11 +1300

    Add dotenv and .env for environment variables

--- config.py
+++ config.py
@@ -1,12 +1,13 @@
 import os
+from dotenv import load_dotenv
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-
+load_dotenv(os.path.join(basedir, '.env'))
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'banana-banana-banana'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'questioner.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     POSTS_PER_PAGE = 3
     LANGUAGES = ['en', 'es']
-    MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY') or 'b749f32a529d47f59434472543146841'
+    MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
