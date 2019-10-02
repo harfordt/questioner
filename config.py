abab1ce539c10a138b27eeb8aff69b10367dc9fb
harfordt <tim.harford@wgc.school.nz>
2019-10-02 14:15:04 +1300

    Add constants relating to pagination and translation

--- config.py
+++ config.py
@@ -7,3 +7,6 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'banana-banana-banana'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'questioner.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    POSTS_PER_PAGE = 3
+    LANGUAGES = ['en', 'es']
+    MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY') or 'b749f32a529d47f59434472543146841'
